0bc7d2c34f5396c363db2957b5ed7d9a7d2e628c
Richard Tan <richard.tanadi99@gmail.com>
2024-11-07 21:23:51 +0700

    first real push market py

--- market.py
+++ market.py
@@ -1,36 +1,146 @@
-class FruitMarket:
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
-        self.inventory = {}
+# fruit market program v.0.0.1
+# initialize variables
 
-    def add_fruit(self, fruit_name, quantity, price_per_unit):
-        if fruit_name in self.inventory:
-            self.inventory[fruit_name]['quantity'] += quantity
-        else:
-            self.inventory[fruit_name] = {'quantity': quantity, 'price_per_unit': price_per_unit}
+# Define fruit database
+database_buah = [
+    {
+        "nama":"Apel",
+        "harga":10000,
+        "stok":5,
+        "cart":None,
+    },
+    {
+        "nama":"Jeruk",
+        "harga":15000,
+        "stok":7,
+        "cart":None,
+    },
+    {
+        "nama":"Anggur",
+        "harga":20000,
+        "stok":9,
+        "cart":None,
+    }
+]
 
-    def remove_fruit(self, fruit_name, quantity):
-        if fruit_name in self.inventory:
-            if self.inventory[fruit_name]['quantity'] >= quantity:
-                self.inventory[fruit_name]['quantity'] -= quantity
+menu=0
+while menu!=6:
+    print("Selamat datang di Pasar Buah")
+    print("List Menu: ")
+    print("1. Menampilkan Daftar Buah")
+    print("2. Menambah Buah")
+    print("3. Menghapus Buah")
+    print("4. Update Buah")
+    print("5. Membeli Buah")
+    print("6. Exit Program")
+    try:
+        menu = int(input("Masukkan input Menu, 1-6: "))
+        if menu == 1:
+            # print out daftar buah / READ
+            # print header
+            print("|Index\t | Nama  \t | Harga\t | Stok\t | Cart\t |")
+            for i in range(0, len(database_buah)):
+                print(f"|{i}\t | {database_buah[i]['nama']}  \t | {database_buah[i]['harga']}\t | {database_buah[i]['stok']}\t | {database_buah[i]['cart']}\t |")
+        elif menu == 2:
+            #menambah buah / CREATE
+            #memeriksa nama buah lama
+            nama_buah_lama = [buah['nama'] for buah in database_buah]
+
+            #meminta input  
+            nama_buah_baru = input("Masukkan Nama Buah Baru: ")
+            #validasi apakah nama buah baru ada di database
+            if nama_buah_baru in nama_buah_lama:
+                print("Buah sudah ada! ")
             else:
-                raise ValueError("Not enough fruit in stock.")
+                harga_buah_baru = int(input("Masukkan Harga Buah Baru: "))
+                stok_buah_baru = int(input("Masukkan Stok Buah Baru: "))
+                database_buah.append({
+                    "nama":nama_buah_baru,
+                    "harga":harga_buah_baru,
+                    "stok":stok_buah_baru,
+                    "cart":100,
+                })
+        elif menu == 3:
+            #menghapus buah / DELETE
+            for i in range(0, len(database_buah)):
+                print(f"Buah tersedia: {database_buah[i]['nama']}, Indeks: {i} ")
+            index_buah_to_be_deleted = int(input("Masukkan Index Buah Ingin Dihapus: "))
+            del database_buah[index_buah_to_be_deleted]
+        elif menu == 4:
+            #update buah/ UPDATE
+            for i in range(0, len(database_buah)):
+                print(f"Buah tersedia: {database_buah[i]['nama']}, Indeks: {i} ")
+            index_buah_to_be_updated = int(input("Masukkan Index Buah Ingin Diperbarui: "))
+            #serve previous information to the user
+            print(f"Buah yang akan diperbarui: {database_buah[index_buah_to_be_updated]['nama']}")
+            print(f"Harga Buah sekarang: {database_buah[index_buah_to_be_updated]['harga']}")
+            harga_buah_baru = int(input("Masukkan Harga Baru: "))
+            if harga_buah_baru == 0 :
+                pass
+            else:
+                database_buah[index_buah_to_be_updated]['harga'] = harga_buah_baru
+
+            print(f"Stok Buah sekarang: {database_buah[index_buah_to_be_updated]['stok']}")
+            stok_buah_baru = int(input("Masukkan Stock Buah Baru: "))
+            if stok_buah_baru == 0 :
+                pass
+            else:
+                database_buah[index_buah_to_be_updated]['stok'] = stok_buah_baru
+        elif menu == 5:
+            for i in range(0, len(database_buah)):
+                while database_buah[i]['cart'] is None:
+                    print(f"Jumlah {database_buah[i]['nama']} yang tersedia: {database_buah[i]['stok']}")
+                    try:
+                        temp_jumlah_buah = int(input(f"Masukkan Jumlah {database_buah[i]['nama']} : "))
+                        if temp_jumlah_buah< database_buah[i]['stok']:
+                            database_buah[i]['cart'] = temp_jumlah_buah
+                        else:
+                            print("Kelebihan!")
+                    except Exception as e:
+                        print(e)
+                        print("Input tidak benar!")
+               
+            grand_total = 0
+            for i in range(0, len(database_buah)):
+                database_buah[i]['total_belanja'] = database_buah[i]['cart'] * database_buah[i]['harga']
+                grand_total = grand_total + database_buah[i]['total_belanja']
+
+            # Print the shopping cart details
+            print("\nDetail Belanja")
+            for i in range(0, len(database_buah)):
+                print(f"{database_buah[i]['nama']} : {database_buah[i]['cart']} x {database_buah[i]['harga']} = {database_buah[i]['total_belanja']}")
+            if grand_total == 0: 
+                pass
+            else:
+                print("\nTotal :", grand_total)
+
+                #read customer payment amount
+                jumlah_pembayaran = int(input("Masukkan jumlah uang : "))
+                if jumlah_pembayaran>grand_total:
+                    print("Terimakasih")
+                    print("Kembalian anda ",jumlah_pembayaran-grand_total)
+
+                    for i in range(0, len(database_buah)):
+                        database_buah[i]['stok'] = database_buah[i]['stok'] - database_buah[i]['cart']
+
+
+                elif jumlah_pembayaran==grand_total:
+                    print("Terimakasih")
+                    for i in range(0, len(database_buah)):
+                        database_buah[i]['stok'] = database_buah[i]['stok'] - database_buah[i]['cart']
+                else:
+                    print("Pembelian anda dibatalkan")
+                    print("Uang anda Kurang : ",abs(grand_total-jumlah_pembayaran))
+
+            for i in range(0, len(database_buah)):
+                database_buah[i]['cart'] = None
+
         else:
-            raise ValueError("Fruit not found in inventory.")
+            pass
 
-    def get_inventory(self):
-        return self.inventory
+    except Exception as e:
+        print(e)
+        print("Input tidak benar!")
 
-    def get_total_value(self):
-        total_value = 0
-        for fruit, details in self.inventory.items():
-            total_value += details['quantity'] * details['price_per_unit']
-        return total_value
 
-market = FruitMarket("Fruit Market", "Jl. Buah")
-market.add_fruit("Apple", 10, 2000)
-market.add_fruit("Orange", 5, 1500)
-market.remove_fruit("Apple", 3)
-print(market.get_inventory())
-print(market.get_total_value())
+# 
